fb620485d148b9a9137f9897a958a61c77074a69
ACER <ACER@LAPTOP-CJR7DT6D>
2023-05-27 19:49:58 +0800

    Katalaon Data Driven Testing Website https://www.saucedemo.com/

---
+++ Test Suites/AddToCart and RemoveFromCart.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>AddToCart and RemoveFromCart</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ea30a034-bff9-4a6b-abc0-5f29e70a43a1</testSuiteGuid>
+   <testCaseLink>
+      <guid>40d373de-d3e4-411e-b550-a755b0e35266</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Swag Lab/AddToCart</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f65bee03-cd8c-4391-b13a-125950d544ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Swag Lab/Remove from Cart</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/LoginforManyUser.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginforManyUser</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>688aa9ab-cbdc-4420-a9dc-8f055fd4a865</testSuiteGuid>
+   <testCaseLink>
+      <guid>d520389a-040f-4a7e-a36b-9fd0560f7873</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Swag Lab/TC001-Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>52ea6dcc-d935-404c-9ca2-e757d0ea130e</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>52ea6dcc-d935-404c-9ca2-e757d0ea130e</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>b661ec47-f602-470d-bfb4-14939d5e231e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>52ea6dcc-d935-404c-9ca2-e757d0ea130e</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>915e82c1-2a6b-4812-a025-e78728ee59e0</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
